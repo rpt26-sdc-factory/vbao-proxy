1ce807a2d89fe546ccf845efd5d138f43b9d6919
vbaobao <vbao.main@gmail.com>
2021-06-15 01:47:12 -0700

    update script to use services object

--- server/templates/scripts.js
+++ server/templates/scripts.js
@@ -1,13 +1,13 @@
 module.exports = (services) => `
   <script crossorigin src="https://unpkg.com/react@17/umd/react.production.min.js"></script>
   <script crossorigin src="https://unpkg.com/react-dom@17/umd/react-dom.production.min.js"></script>
-  ${services.map((service) => `<script src="/services/${service}.js"></script>`).join('\n')}
+  ${Object.keys(services).map((service) => `<script src="/public/services/${service}.js"></script>`).join('\n')}
 
   <script>
-    ${services.map((service) => `
+    ${Object.keys(services).map((service) => `
       ReactDOM.hydrate(
         React.createElement(${service}),
-        document.getElementById(${service.toLowerCase()})
+        document.getElementById('${service}')
       );
     `)}
   </script>
