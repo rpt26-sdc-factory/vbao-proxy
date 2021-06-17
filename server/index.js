63b73581ec45c29ad566673d1e0855267bab5cf1
vbaobao <vbao.main@gmail.com>
2021-06-16 20:10:26 -0700

    Remove loaderio endpoint and add caching

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const { client, checkCache } = require('./cache.js');
 
 const app = express();
 const PORT = 3000;
@@ -15,7 +16,6 @@ if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 const clientBundles = './public/services';
 const serviceConfig = require('./service-config.json');
 const loader = require('./loader.js');
-const loaderio = require('../__stress_tests__/loaderio.js');
 
 const HTML = require('./templates/layout');
 const ComponentTags = require('./templates/app');
@@ -35,14 +35,16 @@ app.use(express.static(path.resolve(__dirname, '../public')));
 app.use(`/${process.env.LOADERIO}.txt`, express.static(path.join(__dirname, `../${process.env.LOADERIO}.txt`)));
 
 app.get('/favicon.ico', (req, res) => res.status(204).end());
-app.get('/loaderio', loaderio);
 
-app.get('/:id', async (req, res) => {
+app.get('/:id', checkCache, async (req, res) => {
   const data = await loader(clientBundles, serviceConfig, req.params.id);
   const services = renderComponents(data);
   const divs = ComponentTags(services);
   const scripts = ScriptTags(services);
-  res.end(HTML(divs, scripts));
+  const results = HTML(divs, scripts);
+
+  client.setex(req.params.id, 3600, results);
+  res.end(results);
 });
 
 app.listen(PORT, () => {
